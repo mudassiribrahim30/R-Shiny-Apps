974366ca1da53907cb60766046dbb2f54c450d99
Mudasir Mohammed Ibrahim <mudassiribrahim30@gmail.com>
2026-03-22 14:23:19 -0700

    Update code to run before deployment.R

--- code to run before deployment.R
+++ code to run before deployment.R
@@ -1,5 +1,5 @@
 # Fix Shinyapps.io timeout issue
-options(shinyapps.http_timeout = 900)   # 5 minutes
+options(shinyapps.http_timeout = 9000)   
 options(timeout = 900)                  # General R timeout
 
 library(curl)
@@ -7,15 +7,14 @@ library(curl)
 safe_api_call <- function(url) {
   h <- new_handle()
   handle_setopt(h,
-                timeout = 900,          # 5 minutes
-                connecttimeout = 120     # 1 minute
+                timeout = 9000,         
+                connecttimeout = 1800920     
   )
   curl_fetch_memory(url, handle = h)
 }
 
 
 res <- safe_api_call("https://api.shinyapps.io/your_endpoint")
-
 
 
 
